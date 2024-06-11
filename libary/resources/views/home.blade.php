a106642f89e7f048772e46a00fd20c291896b429
windspiel <jojo_manni-magi@gmx.de>
2024-06-11 09:05:06 +0200

    Basic Styles and User View

--- libary/resources/views/home.blade.php
+++ libary/resources/views/home.blade.php
@@ -11,55 +11,69 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        <style>
-        </style>
+        <link href="css/libaryStyle.css" rel="stylesheet" />
     </head>
 <body>
     <div id="header">
-        <h1>LIBARY</h1>
+        
+        
         <ul>
-            <li class="underlined">
-                <p>BORROW</p>
-            </li>
-            <li >
-                <p>RETURN</p>
+            <li id="logo">
+                <h1 >LIBARY</h1>
             </li>
             <li>
-                <p>ADMIN</p>
+                <a class="nav-link selected">BORROW</a>
+            </li>
+            <li>
+                <a class="nav-link">RETURN</a>
+            </li>
+            <li>
+                <a class="nav-link">ADMIN</a>
+            </li>
+            <li>
+                <img src="img_girl.jpg" alt="Login" id="userIcon">
             </li>
         </ul>
-        <img src="img_girl.jpg" alt="Girl in a jacket" id="userIcon">
+        
     </div>
-
     <div class="container text-center">
         <div class="row">
-          <div class="col">
-            <h2 class="bookTitle">How to be succesful</h2>
-            <a class="autor"> John Doe</a>
-            <a class="release"> 12.06.1998</a>
-          <div class= available> 
-            <img class = "availableImg"> 
-            <a>
-                available
-            </a> 
-            <button>
-                Borrow
-            </button>
+            <div class="col">
+                <div class="bookItem">
+                    <h2 class="bookTitle">How to be succesful</h2>
+                    <a class="autor"> John Doe</a>
+                    <a class="release"> 12.06.1998</a>
+                    <div class= notAvailable> 
+                    <img class = "notAvailableImg"> 
+                        <a> not available </a> 
+                        <button> Borrow </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="bookItem">
+                    <h2 class="bookTitle">How to be succesful</h2>
+                    <a class="autor"> John Doe</a>
+                    <a class="release"> 12.06.1998</a>
+                    <div class= notAvailable> 
+                        <img class = "notAvailableImg"> 
+                        <a> not available </a>
+                    </div>
+                    <button> Borrow </button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="bookItem">
+                    <h2 class="bookTitle">How to be succesful</h2>
+                    <a class="autor"> John Doe</a>
+                    <a class="release"> 12.06.1998</a>
+                    <div class= notAvailable> 
+                    <img class = "notAvailableImg"> 
+                        <a> not available </a> 
+                        <button> Borrow </button>
+                    </div>
+                </div>
+            </div>
         </div>
-
-
-          </div>
-          <div class="col">
-            Book2
-          </div>
-          <div class="col">
-            Book3
-          </div>
-        </div>
-      </div>
-
- <div id="bookshelve">
-
-
- </div>
+    </div>
 </body>
